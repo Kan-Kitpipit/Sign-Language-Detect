9458f93a8a6e8cde0651d22dce8e0a56ff5a6654
Kan-Kitpipit <kan288095@gmail.com>
2025-08-24 14:54:54 +0700

    Add Landmarks

--- sign_collect.py
+++ sign_collect.py
@@ -3,16 +3,41 @@ import numpy as np
 import os
 import mediapipe as mp
 
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+def mediapipe_detection(image_bgr, model):
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+    image_rgb.flags.writeable = False
+    results = model.process(image_rgb)
+    image_rgb.flags.writeable = True
+    image_bgr = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
+    return results
+
+def draw_landmarks(image, results):
+    # Face
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+    # Pose
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    # Left Hand
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    # Right Hand
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
 cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
 
-    cv2.imshow('Camera', frame)
+        results = mediapipe_detection(frame, holistic)
+        draw_landmarks(frame, results)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow('Camera', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
