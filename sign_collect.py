fb94a4c238b926f156c62e0729c12431f2036e99
Kan-Kitpipit <kan288095@gmail.com>
2025-08-24 15:19:54 +0700

    Add Keypoints

--- sign_collect.py
+++ sign_collect.py
@@ -12,7 +12,7 @@ def mediapipe_detection(image_bgr, model):
     results = model.process(image_rgb)
     image_rgb.flags.writeable = True
     image_bgr = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
-    return results
+    return image_bgr, results
 
 def draw_landmarks(image, results):
     # Face
@@ -24,6 +24,26 @@ def draw_landmarks(image, results):
     # Right Hand
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
+def extract_keypoints(results) -> np.ndarray:
+    pose = np.zeros(33*4, dtype=np.float32)     # 132
+    face = np.zeros(468*3, dtype=np.float32)    # 1404
+    lh = np.zeros(21*3, dtype=np.float32)       # 63
+    rh = np.zeros(21*3, dtype=np.float32)       # 63
+
+    if results.pose_landmarks:
+        pose = np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in results.pose_landmarks.landmark], dtype=np.float32).flatten()
+    
+    if results.face_landmarks:
+        face = np.array([[landmark.x, landmark.y, landmark.z] for landmark in results.face_landmarks.landmark], dtype=np.float32).flatten()
+
+    if results.left_hand_landmarks:
+        lh = np.array([[landmark.x, landmark.y, landmark.z] for landmark in results.left_hand_landmarks.landmark], dtype=np.float32).flatten()
+
+    if results.right_hand_landmarks:
+        rh = np.array([[landmark.x, landmark.y, landmark.z] for landmark in results.right_hand_landmarks.landmark], dtype=np.float32).flatten()
+
+    return np.concatenate([pose, face, lh, rh]) # 1662 ค่าต่อเฟรม
+
 cap = cv2.VideoCapture(0)
 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
@@ -31,10 +51,13 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
 
-        results = mediapipe_detection(frame, holistic)
-        draw_landmarks(frame, results)
+        image, results = mediapipe_detection(frame, holistic)
+        draw_landmarks(image, results)
 
-        cv2.imshow('Camera', frame)
+        kpts = extract_keypoints(results)
+        cv2.putText(image, f'Keypoints: {kpts.shape[0]}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        cv2.imshow('Camera', image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
